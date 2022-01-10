2d65a9f45cd7e3668eec87843d826d6590d94bd7
zrq <zrq@163.com>
2022-01-10 13:47:48 +0800

    first commit

---
+++ Klaser/app/src/main/java/com/app/klaser/MainActivity.java
@@ -0,0 +1,17 @@
+package com.app.klaser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 首页
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
